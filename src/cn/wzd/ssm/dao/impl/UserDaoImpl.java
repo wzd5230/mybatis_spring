298535e2da054100cc41c4471488a68b6e2da33b
wzd5230-E420 <772461552@qq.com>
2016-08-26 00:17:26 +0800

    dao implements class extends SqlSessionDaoSupport.

--- src/cn/wzd/ssm/dao/impl/UserDaoImpl.java
+++ src/cn/wzd/ssm/dao/impl/UserDaoImpl.java
@@ -1,27 +1,30 @@
 package cn.wzd.ssm.dao.impl;
 
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 import cn.wzd.ssm.dao.UserDao;
 import cn.wzd.ssm.po.User;
 
-public class UserDaoImpl implements UserDao {
+public class UserDaoImpl extends SqlSessionDaoSupport implements UserDao {
 	
-	private SqlSessionFactory sqlSessionFactory;
-	
-	public UserDaoImpl(SqlSessionFactory sqlSessionFactory){
-		this.sqlSessionFactory = sqlSessionFactory;
-	}
+//	private SqlSessionFactory sqlSessionFactory;
+//	
+//	public UserDaoImpl(SqlSessionFactory sqlSessionFactory){
+//		this.sqlSessionFactory = sqlSessionFactory;
+//	}
 	
 	//通过id获取user对象
 	@Override
 	public User findUserById( int id ) throws Exception{
-		SqlSession session = sqlSessionFactory.openSession();
+		SqlSession session = this.getSqlSession();
 		
 		User user = session.selectOne("cn.wzd.ssm.mapper.UserMapper.findUserById", id);
 		
-		session.close();
+		/**
+		 * Spring 管理 SqlSession后，不允许认为关闭session，Spring会自动关闭SqlSession
+		 */
+		//session.close();
 		
 		return user;
 	}
